58ffdcf56bbc2cb73de94f2441b15c6583e26c9a
Ian Cherabier <iancher06@hotmail.fr>
2022-11-16 21:06:50 +0100

    (De)Activate gun image (current laser) in UI when (un)equipping

--- Assets/Scripts/WeaponUI.cs
+++ Assets/Scripts/WeaponUI.cs
@@ -1,11 +1,8 @@
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
 
 public class WeaponUI : MonoBehaviour
 {
-    [SerializeField] Image weaponImage = null;
-    [SerializeField] TextMeshProUGUI ammoText = null;
+    [SerializeField] Transform weaponVisualsLocation = null;
 
     private void Start()
     {
@@ -14,7 +11,55 @@ public class WeaponUI : MonoBehaviour
 
     public void SwitchWeaponTo(GameObject weapon)
     {
-        if (weapon == null) Debug.Log("Unequip weapon");
-        else Debug.Log("Equip " + weapon.name);
+        if (weapon == null)
+        {
+            DeactivateCurrentWeaponVisual();
+        }
+        else
+        {
+            bool weaponVisualsAlreadyExists = ActivateWeaponVisualIfAlreadyExists(weapon);
+            if (!weaponVisualsAlreadyExists) InstantiateNewWeaponVisual(weapon);
+        }
+    }
+
+    private void DeactivateCurrentWeaponVisual()
+    {
+        foreach (Transform child in weaponVisualsLocation)
+        {
+            if (child.gameObject.activeSelf)
+            {
+                child.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private bool ActivateWeaponVisualIfAlreadyExists(GameObject weapon)
+    {
+        bool weaponVisualsAlreadyExists = false;
+        foreach (Transform child in weaponVisualsLocation)
+        {
+            if (child.name == weapon.transform.GetChild(0).name)
+            {
+                child.gameObject.SetActive(true);
+                weaponVisualsAlreadyExists = true;
+                break;
+            }
+        }
+
+        return weaponVisualsAlreadyExists;
+    }
+
+    private void InstantiateNewWeaponVisual(GameObject weapon)
+    {
+        GameObject weaponInstance = Instantiate(
+                        weapon.transform.GetChild(0).gameObject,
+                        weaponVisualsLocation);
+
+        weaponInstance.layer = LayerMask.NameToLayer("UI");
+        weaponInstance.name = weapon.transform.GetChild(0).name;
+
+        weaponInstance.transform.localRotation = Quaternion.identity;
+        weaponInstance.transform.localPosition = Vector3.zero;
+        weaponInstance.transform.localScale = Vector3.one;
     }
 }
