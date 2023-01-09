d12f0e5e66539e4a7f09ba348c1d0437ad977164
Armando-Navarrete <viking805fan@yahoo.com>
2023-01-09 11:05:45 -0800

    Car Selection Scene, UI & Scripts

---
+++ Assets/Scripts/ScriptableObjectChanger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class ScriptableObjectChanger : MonoBehaviour
+{
+    [SerializeField] private ScriptableObject[] scriptableObjects;
+
+    [Header ("Display Scripts")]
+    [SerializeField] private CarDisplay carDisplay;
+    private int currentMapIndex;
+
+    private void Awake()
+    {
+        ChangeMap(0);
+    }
+
+    public void ChangeMap(int _index)
+    {
+        currentMapIndex += _index;
+        if (currentMapIndex < 0) currentMapIndex = scriptableObjects.Length - 1;
+        if (currentMapIndex > scriptableObjects.Length - 1) currentMapIndex = 0;
+        
+        if(carDisplay != null) carDisplay.UpdateCar((Car)scriptableObjects[currentMapIndex]);
+    }
+}
