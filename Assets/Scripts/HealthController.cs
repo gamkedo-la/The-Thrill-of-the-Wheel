22887e43b1d5a7f576123031ec6ae7ccac241a7e
Luis Montaña <LuisMontana@users.noreply.github.com>
2023-03-12 13:08:49 +0100

    Added functionality to destroy cars when health falls below 1

--- Assets/Scripts/HealthController.cs
+++ Assets/Scripts/HealthController.cs
@@ -6,8 +6,8 @@ public class HealthController : MonoBehaviour
     
     [SerializeField] private int _maxHealth;
     [SerializeField] private int _currentHealth;
+    [SerializeField] private GameObject _deathPrefab;
     public event Action<float> onHealthChange;
-    
     // this should be deleted later on
     private void Start() {
     }
@@ -15,8 +15,13 @@ public class HealthController : MonoBehaviour
     public void ChangeLife(int change) {
         _currentHealth += change;
         Debug.Log(_currentHealth);
-        float percentageChange = (float) change / _maxHealth;
+        if(_currentHealth < 1) {
+            AudioManager.Instance.PlaySFX("Explosion");
+            Instantiate(_deathPrefab, transform.position, Quaternion.identity);
+            gameObject.SetActive(false);
+        }
         if(gameObject.CompareTag("Player")) {
+            float percentageChange = (float) change / _maxHealth;
             if (onHealthChange!=null) onHealthChange(percentageChange);
         }
     }
