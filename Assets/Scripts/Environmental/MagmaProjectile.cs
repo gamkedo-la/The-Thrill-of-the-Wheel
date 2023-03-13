cb4338949109bfe1f92014531b317a96673c9dcd
PatThompson88 <patrick.thompson688@gmail.com>
2023-03-13 16:33:43 -0400

    Adds explosion VFX and collision behavior to MagmaBoulder

--- Assets/Scripts/Environmental/MagmaProjectile.cs
+++ Assets/Scripts/Environmental/MagmaProjectile.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class MagmaProjectile : MonoBehaviour
 {
-    private void OnCollisionStay(Collision other) {
+    public GameObject impactVFXPrefab;
+    private void OnCollisionStay(Collision other)
+    {
         Invoke("Despawn", 4f);
     }
-
-    private void Despawn() {
+    private void OnCollisionEnter(Collision other)
+    {
+        // if the magma ball hits something:
+        // get impact point
+        Instantiate(impactVFXPrefab, other.GetContact(0).point, Quaternion.identity);
+        Despawn();
+        // instantiate vfx at impact point
+    }
+    private void Despawn()
+    {
+        // instantiate vfx at impact point
         Destroy(gameObject);
     }
 }
