e3da1980a373ec692a479c3dc4649d2decae8fd4
Luis Montaña <LuisMontana@users.noreply.github.com>
2023-03-17 13:02:53 +0100

    Add endscreen logic

---
+++ Assets/Scripts/Level Management/EndScreen.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+using UnityEngine.SceneManagement;
+
+public class EndScreen : MonoBehaviour
+{
+    [SerializeField] GameObject winText;
+    [SerializeField] GameObject loseText;
+    bool isLose;
+    // Start is called before the first frame update
+    void Start()
+    {
+        string endState = PlayerPrefs.GetString("EndState");
+        Debug.Log(endState);
+        if(endState == "Win") {
+            winText.SetActive(true);
+        } else {
+            isLose = true;
+            loseText.SetActive(true);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isLose) {
+            InputSystem.onAnyButtonPress.CallOnce(ctrl => SceneManager.LoadScene("CarSelection"));
+        } else {
+            InputSystem.onAnyButtonPress.CallOnce(ctrl => Application.Quit());
+        }
+    }
+}
